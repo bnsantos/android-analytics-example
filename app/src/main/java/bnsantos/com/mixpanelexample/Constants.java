d077adc1d9d019dc377d7fb21797825e2cee89fd
Bruno Santos <brunonascimentosantos@gmail.com>
2014-10-22 15:41:10 -0200

    main activity basic functionality

--- app/src/main/java/bnsantos/com/mixpanelexample/Constants.java
+++ app/src/main/java/bnsantos/com/mixpanelexample/Constants.java
@@ -1,9 +1,13 @@
 package bnsantos.com.mixpanelexample;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
 /**
  * Created by bruno on 22/10/14.
  */
 public class Constants {
+    public static final String ISO_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'";
     private static String CURRENT_USER;
     private static String MIXPANEL_TOKEN = ""; //Set your default token here
 
@@ -21,5 +25,10 @@ public class Constants {
 
     public static void setMixpanelToken(String MIXPANEL_TOKEN) {
         Constants.MIXPANEL_TOKEN = MIXPANEL_TOKEN;
+    }
+
+    public static String formatDate(Date calendar){
+        SimpleDateFormat sdf = new SimpleDateFormat(ISO_DATE_FORMAT);
+        return sdf.format(calendar);
     }
 }
--- app/src/main/java/bnsantos/com/mixpanelexample/MainActivity.java
+++ app/src/main/java/bnsantos/com/mixpanelexample/MainActivity.java
@@ -4,33 +4,105 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.mixpanel.android.mpmetrics.MixpanelAPI;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+
 import bnsantos.com.mixpanelexample.R;
 
 public class MainActivity extends Activity {
+    private MixpanelAPI mMixpanelAPI;
+    private TextView mTitle;
+    private ArrayAdapter<CharSequence> mHistoryAdapter;
+    private ListView mHistoryListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mMixpanelAPI = MixpanelAPI.getInstance(this, Constants.getMixpanelToken());
+
+        initViews();
+        initListeners();
+        initAdapter();
     }
 
+    private void initViews(){
+        mTitle = (TextView) findViewById(R.id.loggedAs);
+        mTitle.setText(getString(R.string.logged_as, Constants.getCurrentUser()));
+        mHistoryListView = (ListView) findViewById(R.id.historyListView);
+    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+    private void initListeners(){
+        findViewById(R.id.projectButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addProject();
+            }
+        });
+        findViewById(R.id.noteButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addNote();
+            }
+        });
+        findViewById(R.id.commentButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addComment();
+            }
+        });
+    }
+
+    private void initAdapter(){
+        mHistoryAdapter = new ArrayAdapter<CharSequence>(this, android.R.layout.simple_list_item_1, new ArrayList<CharSequence>());
+        mHistoryListView.setAdapter(mHistoryAdapter);
+    }
+
+    private void addProject(){
+        addItemIntoAdapter(OperationType.PROJECT);
+    }
+    private void addNote(){
+        addItemIntoAdapter(OperationType.NOTE);
+    }
+    private void addComment(){
+        addItemIntoAdapter(OperationType.COMMENT);
+    }
+
+    private void addItemIntoAdapter(OperationType operationType){
+        String operation = Constants.formatDate(Calendar.getInstance().getTime()) + ";" + Constants.getCurrentUser() + ";" + operationType.name();
+
+        //TODO persist it
+        mHistoryAdapter.add(operation);
+        mHistoryAdapter.notifyDataSetChanged();
+
+        showToast(operationType);
+        sendMixPanelEvent();
+    }
+
+    private void showToast(OperationType operationType){
+        Toast.makeText(this, getString(R.string.successfully_added, operationType.name()), Toast.LENGTH_SHORT).show();
+    }
+
+    private void sendMixPanelEvent(){
+        //TODO
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    protected void onDestroy() {
+        super.onDestroy();
+        mMixpanelAPI.flush();
+    }
+
+    private enum OperationType{
+        PROJECT, NOTE, COMMENT
     }
 }
